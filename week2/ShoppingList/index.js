6a5342b0add7bac7e4750d448abf39a1844447fb
ocahs9 <piorag99@gmail.com>
2024-04-14 18:21:29 +0900

    Feat: 카테고리별 렌더링 구현, 장바구니 담기 기능 구현

--- week2/ShoppingList/index.js
+++ week2/ShoppingList/index.js
@@ -6,89 +6,154 @@ const albumSection = document.querySelector('#album');
 const movieSection = document.querySelector('#movie');
 const travelSection = document.querySelector('#travel');
 
-//console.log(allSection, albumSection, movieSection, travelSection); //정상적으로 가져오는지 확인
+const mainContainer = document.querySelector("#mainContainer");
+
+// 매번 렌더링시마다 filter해서 요소들을 걸러내는게 비효율적이라 판단.
+// 따라서 미리 필터링 된 요소들을 저장해 둠.
+// 참고로, const로 선언한 배열 자체는 수정(대체)이 불가능하지만,
+// 배열의 '요소'들은 수정이 가능하다.
+const allObjects = ITEMLIST;
+const albumObjects = ITEMLIST.filter((obj) => obj.category === "album"); 
+const movieObjects = ITEMLIST.filter((obj) => obj.category === "movie"); 
+const travelObjects = ITEMLIST.filter((obj) => obj.category === "travel"); 
+
+function removeOtherSection(){
+  const all = document.getElementById("all");
+  const album = document.getElementById("album");
+  const movie = document.getElementById("movie");
+  const travel = document.getElementById("travel");
+
+  if(all !== null) //해당 섹션이 존재한다면,
+  {
+    all.remove(); //해당 섹션 요소 삭제
+    console.log("all 섹션 삭제됨");
+  }
+  else if(album !== null) //else if를 사용한 이유 -> 어차피 하나의 섹션만 렌더링되어 있을것이므로!
+  {
+    album.remove();
+    console.log("album 섹션 삭제됨");
+  }
+  else if(movie !== null)
+  {
+    movie.remove();
+    console.log("movie 섹션 삭제됨");
+  }
+  else if(travel !== null)
+  {
+    travel.remove();
+    console.log("travel 섹션 삭제됨");
+  }
+
+}
+
+//item 선택시 alert 창 뜨게 하는 로직(이벤트 리스너)
+function addCart(e){
+  if(confirm("장바구니에 추가하시겠습니까?")){ //yes를 선택할 시 
+    //해당 타겟의 id와 같은 id를 갖는 객체의 인덱스를 탐색 (참고 : find는 객체 자체를 반환)
+    const findObjIdx = allObjects.findIndex((obj) => obj.id === parseInt(e.currentTarget.id)); 
+    allObjects[findObjIdx].cart = true; //해당 배열에서의 객체 cart 속성을 true로 설정
+
+    //console.log(allObjects);
+  }
+  else{ //no를 선택할 시
+    console.log("취소!");
+  }
+}
+
+const renderSection = (id, title, arr) =>{
+  removeOtherSection();
+  
+  //섹션을 만들고, 제목까지 붙이는 로직
+  const section = document.createElement("section"); //스트링을 인자로...!
+  section.classList.add("flexSection"); //setAttribute 사용시 아예 초기화됨. 따라서 덧붙일 수 있도록 classList.add 사용
+  section.setAttribute("id", id); //그냥 section.id = "id명"; 이라해도 같은 효과이긴 하다. (dir로 확인해보니 가능함)
+  mainContainer.appendChild(section);
+
+  const h2 = document.createElement("h2");
+  h2.innerText = title;
+  section.appendChild(h2);
+
+  //console.log(`섹션의 아이디는 : ${section.id} 입니다.`)
+  //console.log(`${h2.innerText}섹션이 렌더링 시작됩니다.`);
+  //console.log("현재 가져온 배열: ", arr);
+  arr.forEach((obj)=>{
+    const article = document.createElement("article");
+    article.classList.add("productCard");
+    //굳이 id를 추가시키는 이유는, 다른 객체들과 구분하여 장바구니에 추가하기 위한 로직에 활용하기 위함이다.
+    article.setAttribute("id", obj.id); 
+    article.addEventListener("click", addCart); //바로 click 이벤트 붙여놓음
+    section.appendChild(article);
+
+    const img = document.createElement("img");
+    img.classList.add(obj.category);
+    img.setAttribute("src", obj.img);
+    article.appendChild(img);
+
+    const heartIcon = document.createElement("i");
+    heartIcon.classList.add("fa-solid");
+    heartIcon.classList.add("fa-heart");
+    article.appendChild(heartIcon);
+
+    const h4 = document.createElement("h4");
+    h4.innerText = obj.name;
+    article.appendChild(h4);
+
+    const p = document.createElement("p");
+
+    // 천자리 단위로 ,를 찍도록 변환 ! (toString 함수를 안쓸 것!)
+    // 단, 반드시 **숫자형 데이터**에 .toLocaleString()을 써야한다. 
+    //-인자 없으면, 브라우저의 현재 언어 설정을 확인하여 로컬을 결정하고, 그에 맞는 인자를 넣어준다.
+    p.textContent = (obj.price).toLocaleString('ko-KR'); //string을 숫자로 변환하여 넣음, 그리고 실습에서 사용했던 textContent를 사용해봄
+    // https://hianna.tistory.com/441 참고
+    article.appendChild(p);
+    
+  });
+};
+
+renderSection("all", "전체", ITEMLIST);
+
 
 const allList = document.querySelector("#allList");
 const albumList = document.querySelector("#albumList");
 const movieList = document.querySelector("#movieList");
 const travelList = document.querySelector("#travelList");
 
-//console.log(allList, albumList, movieList, travelList);
-//console.dir(allList, albumList, movieList, travelList); //이벤트 뭘 사용할 수 있을지 파악
-
-const closeOtherSection = () => {
-  //console.log("지금 closeOtherSection 함수가 호출되었습니다.");
-  /*********************
-   * console.dir로 확인해보면, 요소.style.diplay 의 값이 null 이다.
-   * 이외에도, 다른 모든 style들의 프로퍼티가 null로 되어있다.
-   * 그 이유는, 기본적으로 위와 같은 .(dot)방식의 접근은, 인라인 스타일 요소만 가져올 수 있기 때문이다.
-   * 따라서, 외부에서 CSS를 적용한 것을 반영하여 프로퍼티를 확인하려면, 
-   * <window.getComputedStyle(요소).프로퍼티> 형식의 메서드를 활용해야 한다 - 공유해놓음
-   *********************/
-    
-  let isAllOpen = window.getComputedStyle(allSection).display; //allSection.style.display; 
-  let isAlbumOpen =  window.getComputedStyle(albumSection).display;//albumSection.style.display;
-  let isMovieOpen =  window.getComputedStyle(movieSection).display;//movieSection.style.display;
-  let isTravelOpen =  window.getComputedStyle(travelSection).display;//travelSection.style.display;
-  //console.log(isAllOpen);
-  //console.log(`${isAllOpen}`);
-  //전시되고 있는 섹션은 안보이게 바꿈
-  if(isAllOpen === "flex")
-  {
-    allSection.style.display = "none"; 
-    console.log("allSection 사라짐!");
-  }
-    
-  if(isAlbumOpen === "flex")
-  {
-    albumSection.style.display = "none";
-    console.log("albumSection 사라짐!");
-  }
-    
-  if(isMovieOpen === "flex")
-  {
-    movieSection.style.display = "none";
-    console.log("movieSection 사라짐!");
-  }
-    
-  if(isTravelOpen === "flex")
-  {
-    travelSection.style.display = "none";
-    console.log("travelSection 사라짐!");
-  }
-  
-}
-
-const openList = (e) => {
-  //console.log("지금 openList 함수가 호출되었습니다.");
-  //console.log(`you click ${e.currentTarget}`);
-  //console.log(e.currentTarget);
-  const list = e.currentTarget; //그냥 target하면 정확히 a 클릭시 자식요소인 해당요소만 인식
-
-  closeOtherSection();
-  //console.log(`지금 클릭한 리스트의 아이디는 ${list.id} 입니다.`); // id 접근가능한가 확인
+/*****처음에 계속 all말고 다른거 렌더링 안되길래 디버깅 계속 해봤는데
+ * 허무하게 예전에 작성해놓은 display:none 속성 때문이었음 - 현재는 css상에서 삭제 완료
+ * 문제가 발생할 시, inspector -> element의 요소들을 활용하여 바로 디버깅해볼 것.
+ * *****/
+//화살표 함수 말고 다른 방법으로도 함수를 정의해봄
+function renderCategory(e){
+  //하나로 통일해서 작성해보려고 시도 중!
+  const list = e.currentTarget //자식 요소 눌렀을 때, 다른 거 안 가져오도록 일부러 current 타겟으로 함
+  removeOtherSection();
   switch(list.id)
   {
     case "allList":
-      allSection.style.display = "flex"; /*중요! 위에서 언급했지만, 이는 인라인 스타일을 적용하는 것이고, 우선순위가 높아진다*/
-      break; //break 꼭 써주기! 처음에 까먹고 안 썼다가 전부 렌더링 됨.
+      console.log("all 섹션 리렌더링 시작");
+      renderSection("all", "전체", allObjects);
+      break; //break 꼭 써주기! 
     case "albumList":
-      albumSection.style.display = "flex";
+      console.log("album 섹션 리렌더링 시작");
+      renderSection("album", "앨범", albumObjects);
       break;
     case "movieList":
-      movieSection.style.display = "flex";
+      console.log("movie 섹션 리렌더링 시작");
+      renderSection("movie", "영화", movieObjects);
       break;
     case "travelList":
-      travelSection.style.display = "flex";
+      console.log("travel 섹션 리렌더링 시작");
+      renderSection("travel", "여행", travelObjects);
       break;
   }
+  //console.log("최종 :", mainContainer);
 }
+allList.addEventListener('click', renderCategory);
+albumList.addEventListener('click', renderCategory); 
+movieList.addEventListener('click', renderCategory); 
+travelList.addEventListener('click', renderCategory);  
 
-//console.dir(allList);
-//잘 동작하긴 하는데 console.dir로 확인했을 때 왜 onclick이 null로 나올까? 
-//코드의 반복인데, 이를 좀 더 간단하게 쓰는 방법은 없을까?
-allList.addEventListener('click', openList);
-albumList.addEventListener('click', openList); 
-movieList.addEventListener('click', openList); 
-travelList.addEventListener('click', openList);  
+
+
+
 
